f90c21987f48e119113aa11e34f7c65d27206513
harsh-05 <harshverma09022001@gmail.com>
2026-01-22 21:14:28 +0530

    Fix: AuthProvider

--- React-client/src/App.tsx
+++ React-client/src/App.tsx
@@ -1,17 +1,21 @@
 import { Route, Routes } from "react-router"
 import Login from "./Login"
 import ProtectedRoute from "./Components/ProtectedRoutes"
+import { Dashboard } from "./Components/Dashboard"
+import { AuthProvider } from "./Components/AuthContext"
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Login></Login>}></Route>
-      <Route element={<ProtectedRoute></ProtectedRoute>}>
-        <Route path="/dashboard" element></Route>
-        {/* Other protected Routes will be followed here...... */}
-      </Route>
-   </Routes>
-  )
+    <AuthProvider>
+      <Routes>
+        <Route index element={<Login></Login>}></Route>
+        <Route element={<ProtectedRoute></ProtectedRoute>}>
+          <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+          {/* Other protected Routes will be followed here...... */}
+        </Route>
+      </Routes>
+    </AuthProvider>
+  );
 }
 
 export default App
